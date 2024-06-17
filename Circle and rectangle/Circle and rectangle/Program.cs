f3b30ee547f1c52981808ce9e8ee5886e1e2eb7f
OsborneSebuabe <ojsebuabe@st.ug.edu.gh>
2024-06-17 08:51:09 +0000

    Assignment 2
    Abstract classes and methods

---
+++ Circle and rectangle/Circle and rectangle/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ShapeAreas
+{
+    // Abstract class Shape
+    abstract class Shape
+    {
+        // Abstract method to be implemented in derived classes
+        public abstract double GetArea();
+    }
+
+    // Derived class Circle
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        // Implement the GetArea() method
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    // Derived class Rectangle
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        // Implement the GetArea() method
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of Circle
+            Circle circle = new Circle(5);
+            Console.WriteLine($"The area of the circle is: {circle.GetArea()}");
+
+            // Create an instance of Rectangle
+            Rectangle rectangle = new Rectangle(4, 6);
+            Console.WriteLine($"The area of the rectangle is: {rectangle.GetArea()}");
+        }
+    }
+}
